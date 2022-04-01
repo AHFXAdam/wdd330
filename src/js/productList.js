892e423f3081bdfd0b18eff707abaf68827dc09e
AHFXAdam <adam@ahfx.net>
2022-04-01 16:15:29 -0600

    fix again

--- src/js/productList.js
+++ src/js/productList.js
@@ -23,8 +23,8 @@ export default class ProductList {
 
   prepareTemplate(clone, product) {
     clone.querySelector('a').href += product.Id;
-    clone.querySelector('img').src = product.Image;
-    clone.querySelector('img').alt += item.Image.replace('../', '');
+    clone.querySelector('img').src = item.Image.replace('../', '');
+    clone.querySelector('img').alt += product.Name;
     clone.querySelector('.card__brand').innerHTML = product.Brand.Name;
     clone.querySelector('.card__name').innerHTML = product.NameWithoutBrand;
     clone.querySelector('.product-card__price').innerHTML += product.ListPrice;
