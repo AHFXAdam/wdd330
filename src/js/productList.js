bf74832159e2940a8fa0c0b46e4cce0c51982288
AHFXAdam <adam@ahfx.net>
2022-04-07 14:34:11 -0600

    add sorting functionality

--- src/js/productList.js
+++ src/js/productList.js
@@ -1,4 +1,5 @@
-import { renderList } from './utils';
+import { doc } from 'prettier';
+import { renderList, getLocalStorage, setLocalStorage } from './utils';
 
 export default class ProductList {
   constructor(dataSource, targetElement, category) {
@@ -8,17 +9,74 @@ export default class ProductList {
   }
 
   async init() {
-    let list = await this.dataSource.getData(this.category);
     // console.log(list);
     //list = this.filterList(list);
-    const template = document.getElementById('product-card-template');
-    renderList(template, this.targetElement, list, this.prepareTemplate);
+    // list = this.sortList(list);
+    this.showList();
     this.updateHeading(this.category);
+    document
+      .querySelector('#sort')
+      .addEventListener('click', (e) => this.sortAndShow());
+    this.updateSortDropdown();
+  }
+
+  updateSortDropdown() {
+    try {
+      const sorting = getLocalStorage('sorting');
+      document.querySelector('#sorting').value = sorting;
+    } catch (error) {
+      console.log('Couldn\'t find sorting');
+    }
+  }
+
+  sortAndShow() {
+    this.setSorting(document.querySelector('#sorting').value);
+    this.showList();
+  }
+
+  async showList() {
+    let list = await this.dataSource.getData(this.category);
+    list = this.sortList(list);
+    const template = document.getElementById('product-card-template');
+    this.targetElement.innerHTML = '';
+    renderList(template, this.targetElement, list, this.prepareTemplate);
+  }
+
+  sortList(list) {
+    console.log(list);
+    const sorting = getLocalStorage('sorting');
+    switch (sorting) {
+      case 'price':
+        console.log('Sort by Price');
+        list.sort((a, b) => (a.FinalPrice > b.FinalPrice ? 1 : -1));
+        break;
+      case 'alpha':
+        console.log('Sort by Alpha');
+        list.sort((a, b) => (a.NameWithoutBrand > b.NameWithoutBrand ? 1 : -1));
+        break;
+      case 'pricedesc':
+        console.log('Sort by Price');
+        list.sort((a, b) => (a.FinalPrice < b.FinalPrice ? 1 : -1));
+        break;
+      case 'alphadesc':
+        console.log('Sort by Alpha');
+        list.sort((a, b) => (a.NameWithoutBrand < b.NameWithoutBrand ? 1 : -1));
+        break;
+      default:
+        console.log('Sort by Default');
+        break;
+    }
+    return list;
+  }
+
+  setSorting(value) {
+    setLocalStorage('sorting', value);
+    console.log(value);
   }
 
   updateHeading(text) {
     document.querySelector(
-      '.products>h2'
+      '.products>div>h2'
     ).innerHTML += ` ${text[0].toUpperCase()}${text
       .slice(1)
       .replace('-', ' ')}`;
